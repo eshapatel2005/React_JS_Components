73bd4a0e7260e990e32bb371fcb5a9b4fd88d730
eshapatel2005 <eshapatel5605@gmail.com>
2026-05-21 14:01:06 +0530

    CSS Styles

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 
 function Header({ color}) {
+  const number = 10
+  const heading = `This is header component with number ${number}`
   return ( 
-  <Navbar fluid rounded>
+  <Navbar fluid rounded className='navbar1'>
       <NavbarBrand href="https://flowbite-react.com">
         <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
@@ -13,9 +15,10 @@ function Header({ color}) {
         <NavbarToggle />
       </div>  
       <NavbarCollapse>
+        <h1 style={{color:'grey'}}>{heading}</h1>
         <NavbarLink href="#" active>Home</NavbarLink>
         <NavbarLink href="#">About</NavbarLink>
-        <NavbarLink href="#" className='txt3'>Services</NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
         <NavbarLink href="#">Pricing</NavbarLink>
         <NavbarLink href="#">Contact</NavbarLink>
       </NavbarCollapse>
--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import styled from "styled-components";
+
 
 function Home({ color }) {
-  console.log("gggggg",color)
+  // Inline style
+  const txt5 = {
+    color:"green",
+    fontSize:"30px",
+    fontWeight:"bold",
+    textAlign:"center",
+    marginTop:"20px"
+  };
+
+  // Styled component
+  const Box = styled.div`
+    background: pink;
+    padding: 20px;
+    border-radius: 10px;
+    color: black;
+    textAlign:center;
+  `;
+
   return (
-    <div className='txt1'>Hello Everyone !!!!!</div>
+    <>
+      <div className='txt1'>Hello Everyone !!!!!</div>
+      <div style={txt5}>Esha Patel S.</div>
+      <table className='tbl1' border="1">
+        <tr>
+          <th>Name </th>
+          <th> Age</th>
+          <th>City</th>
+        </tr>
+
+        <tr>
+          <td>Esha Patel</td>
+          <td>21</td>
+          <td>Surat</td>
+        </tr>
+
+        <tr>
+          <td>Avi Patel</td>
+          <td>15</td>
+          <td>Mumbai</td>
+        </tr>
+      </table>
+      <Box>
+        Hello!!!
+      </Box>
+    </>
+    
   )
 }
 
