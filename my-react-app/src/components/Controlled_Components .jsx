b104bf4cc774b65d5b8e2f63853437e57b0cf4fa
eshapatel2005 <eshapatel5605@gmail.com>
2026-05-21 16:28:51 +0530

    controlled component practice

--- my-react-app/src/components/Controlled_Components .jsx
+++ my-react-app/src/components/Controlled_Components .jsx
@@ -4,7 +4,7 @@ function Controlled_Components () {
     const [username, setUsername] = useState(""); 
     return (
         <>
-            <div>Controlled Component </div><br></br>
+            <div>Controlled Components </div><br></br>
             <input style={{border: '1px solid black'}} value={username} onChange={(e) => setUsername(e.target.value)} /> 
             <p style={{color:"red"}}>{username}</p>
         </>
