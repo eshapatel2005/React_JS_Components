2759ecefaf7765f07fa6bc1887a672e763917c7e
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-22 13:57:47 +0530

    Pure Components

---
+++ my-react-app/src/components/Pure_Components.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Pure_Components() {
+
+  return (
+    <>
+      <h1>Pure Component Example</h1>
+    </>
+  )
+}
+
+export default React.memo(Pure_Components)
