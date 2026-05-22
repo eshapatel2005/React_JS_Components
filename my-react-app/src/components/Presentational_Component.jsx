854b98881db3e48121cdd62eeb0c55912b14c15a
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-22 13:47:38 +0530

    Presentational Component

--- my-react-app/src/components/Presentational_Component.jsx
+++ my-react-app/src/components/Presentational_Component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Presentational_Component({ name, age }) {
   return (
     <>
-        <div>Presentational_Component</div>
+        <div>Presentational_Components</div>
         <h3>{name} - {age} years old</h3>
     </>
   )
